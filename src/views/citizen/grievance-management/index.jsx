e44469e3cd6cd4f757465749268ff15462c1290d
HariHaran212 <hariharan2004121@gmail.com>
2025-07-08 00:31:33 +0530

    Help-desk landing page created

--- src/views/citizen/grievance-management/index.jsx
+++ src/views/citizen/grievance-management/index.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Home from './pages/Home'
+import { Routes, Route } from 'react-router-dom' 
 
 const Grievance_Management = () => {
   return (
-    <div>index</div>
+        <Routes>
+            <Route path='/' element= {<Home /> } />
+            <Route path='complaint-form' element= {null} />
+            <Route path='complaint-log' element= {null} />
+        </Routes>
   )
 }
 
